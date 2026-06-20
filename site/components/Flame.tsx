ac13e926507a7bff82a4a934559a16cf856ea6d2
Sudhanshu Pandit <sudhanshuk1140@gmail.com>
2026-06-20 19:28:16 +0530

    Add site assets and flame logo component

---
+++ site/components/Flame.tsx
@@ -0,0 +1,7 @@
+export function Flame({ className }: { className?: string }) {
+  return (
+    <svg className={className} viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+      <path d="M12 2c1 3 4 4.5 4 8a4 4 0 1 1-8 0c0-1.2.5-2 1-2.8C8 9 8 11 7 12c-1-1-1-3-1-4 0 0-2 2.2-2 6a8 8 0 1 0 16 0c0-5-4-8-8-12z" />
+    </svg>
+  );
+}
